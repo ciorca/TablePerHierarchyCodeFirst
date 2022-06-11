088a71a63fb446bfbc4e4b6a272d213ebd2aeab4
ciorca <ciorca.gaby@gmail.com>
2022-06-11 15:28:04 +0300

    Add project files.

---
+++ TablePerHierarchyCodeFirst/EmployeeDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TablePerHierarchyCodeFirst
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
